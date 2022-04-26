01c610cd938295209d75c34329f575695f2cab2d
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-04-26 14:56:31 -0300

    Create car object

---
+++ scripts/car.js
@@ -0,0 +1,22 @@
+const userCar = {
+  brand: 'Ford',
+  model: 'Focus',
+  version: 'SE Plus',
+  year: 2015,
+  bodyType: 'Hatchback',
+  availableColors: [
+    'Green', 
+    'Red', 
+    'Silver', 
+    'Black', 
+    'Grey', 
+    'Blue'
+  ],
+  currentColor: availableColors[4],
+  engine: '2.0',
+  transmission: 'Powershift 6 marchas + ré',
+  // imagePath: 1234,
+  // dimensions: 1234,
+  // safety: 1234,
+  // performance: 1234,
+};
