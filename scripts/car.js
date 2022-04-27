13ee39d351d3f2f9a0da341f073ca78781cce119
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-04-27 16:04:22 -0300

    Add dimensions info

--- scripts/car.js
+++ scripts/car.js
@@ -4,19 +4,18 @@ const userCar = {
   version: 'SE Plus',
   year: 2015,
   bodyType: 'Hatchback',
-  availableColors: [
-    'Green', 
-    'Red', 
-    'Silver', 
-    'Black', 
-    'Grey', 
-    'Blue'
-  ],
+  availableColors: ['Green', 'Red', 'Silver', 'Black', 'Grey', 'Blue'],
   currentColor: 'Grey',
-  engine:{},
-  transmission:{},
+  engine: {},
+  transmission: {},
   imagePath: 'Image TBD',
-  dimensions:{},
-  safety:{},
-  performance:{},
+  dimensions: {
+    weight: 1375, // In Kilograms
+    height: 1484, // In millimeters
+    width: 1823, // In millimeters
+    length: 4358, // In millimeters
+    betweenAxles: 2648, // In millimeters
+  },
+  safety: {},
+  performance: {},
 };
